a09b111b7e76ed55c970436e5ef5e7aae8849cb2
Shuangli Zhou <shz103@ucsd.edu>
2017-02-15 09:44:40 -0800

    Beautified the add process, num-q change with adding and deleting now

--- routes/managequestion.js
+++ routes/managequestion.js
@@ -2,6 +2,7 @@ delete require.cache[require.resolve('../quiz_data.json')];
 var data = require("../quiz_data.json");
 
 exports.show = function(set, num, req, res){
+
 	console.log("show called");
 	var index = Number(num);
 	console.log("This is question " + index + " in set " + set);
@@ -14,6 +15,10 @@ exports.show = function(set, num, req, res){
 		// alter url property
 		m.quizset[set - 1].questions[i].qurl = 'ques/' + (i + 1) + '/' + set;
 	}
+	var orig = Number(m.quizset[set - 1].num_question);
+	orig -= 1;
+	m.quizset[set - 1].num_question = orig;
+
 	fs.writeFileSync('quiz_data.json', JSON.stringify(m));
 	console.log("JSON wrote back");
 	//delete the fucking cached file
@@ -22,9 +27,11 @@ exports.show = function(set, num, req, res){
 
 	var jumpurl = '/actualshow' + set.toString();
 	res.redirect(jumpurl);
+
 }
 
 exports.adding = function(set, req, res){
+	
 	console.log("add called for " + set);
 	fs = require('fs');
 	var m = JSON.parse(fs.readFileSync('quiz_data.json').toString());
@@ -32,12 +39,18 @@ exports.adding = function(set, req, res){
 	var new_answer = req.query.new_answer;
 	// manipulate json file
 	m.quizset[set - 1].questions.push(
+
 		{
           'qurl' : 'ques/' + (m.quizset[set - 1].questions.length + 1) + '/' + set,
           "q_text" : new_ques,
           "a_text" : new_answer
         }
 		);
+
+	var orig = Number(m.quizset[set - 1].num_question);
+	orig += 1;
+	m.quizset[set - 1].num_question = orig;
+
 	fs.writeFileSync('quiz_data.json', JSON.stringify(m));
 	console.log("JSON wrote back");
 	//delete the fucking cached file
